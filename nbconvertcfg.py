8e40a8dc324d796092fb3ae844c49f561fe4a38e
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2023-01-15 17:22:46 +0100

    fix pdf render by using katex

--- nbconvertcfg.py
+++ nbconvertcfg.py
@@ -1,3 +1,5 @@
 c.NbConvertApp.export_format = "qtpdf"
 c.NbConvertApp.notebooks = ["Dokumentation.ipynb"]
 c.QtExporter.exclude_input = True
+# c.QtExporter.mathjax_url = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+c.QtExporter.mathjax_url = ""  # using KaTeX
